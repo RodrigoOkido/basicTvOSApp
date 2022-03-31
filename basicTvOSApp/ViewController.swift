ca6dabc89bd77f10f9727a2afc314771b7150396
RodrigoOkido <rodrigookido@gmail.com>
2022-03-31 20:30:13 -0300

    Initial Commit

---
+++ basicTvOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicTvOSApp
+//
+//  Created by Rodrigo Yukio Okido on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
